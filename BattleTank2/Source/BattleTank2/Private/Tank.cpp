36fc3444d02fd490dd4b2dd999087d77447ffcd6
Daniel Harvell <danielharvell@gmail.com>
2019-09-29 14:54:06 -0600

    added necessary #include

--- BattleTank2/Source/BattleTank2/Private/Tank.cpp
+++ BattleTank2/Source/BattleTank2/Private/Tank.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "GameFramework/PlayerController.h"
+#include "TankAimingComponent.h"
 #include "Tank.h"
 
 // Sets default values
